e4dab75970b01617ca18a39f936484bb34d424e0
John Herzog <jmherzog1518@gmail.com>
2018-10-07 18:43:54 -0600

    Add project files.

---
+++ IT481_Unit_6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT481_Unit_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
